ee7c328cc691839dcce28ede590b703fd51cfd9e
avojnovicDk <anavojnovic1987@gmail.com>
2021-12-07 21:04:18 +0100

    Slightly less dumb solution

--- calendar/7/solution.py
+++ calendar/7/solution.py
@@ -8,16 +8,36 @@ def solve_pt1(f):
     return sum(abs(p - int(median(positions))) for p in positions)
 
 
-def solve_pt2(f):
-    positions = [int(p) for p in f.readline().split(',')]
-    prev_fuel_cost = None
-    for position in range(min(positions), max(positions) + 1):
-        fuel_cost = sum([sum(range(abs(position - p) + 1)) for p in positions])
-        if prev_fuel_cost and fuel_cost > prev_fuel_cost:
+def _get_min_fuel_cost(prev_fuel_cost, positions_to_check, calc_fuel_cost):
+    for position in positions_to_check:
+        fuel_cost = calc_fuel_cost(position)
+        if fuel_cost > prev_fuel_cost:
             # solution has 1 local minimum, so when we see it
             # increase, we know that answer is prev iter
             return prev_fuel_cost
         prev_fuel_cost = fuel_cost
+
+
+def solve_pt2(f):
+    positions = [int(p) for p in f.readline().split(',')]
+    calc_fuel_cost = lambda x: sum(
+        [sum(range(abs(x - p) + 1)) for p in positions]
+    )
+    median_position = int(median(positions))
+    median_fuel_cost = calc_fuel_cost(median_position)
+    prev_fuel_cost = calc_fuel_cost(median_position + 1)
+    if median_fuel_cost > prev_fuel_cost:
+        return _get_min_fuel_cost(
+            prev_fuel_cost,
+            range(median_position + 2, max(positions) + 1),
+            calc_fuel_cost
+        )
+    else:
+        return _get_min_fuel_cost(
+            median_fuel_cost,
+            range(median_position - 1, -1, -1),
+            calc_fuel_cost
+        )
 
 
 assert solve_pt1(open_file("example.txt")) == 37
